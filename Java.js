c35302419a20c59f1e9da41506ba7ce160d4738a
Hector <leijahector5@gmail.com>
2025-10-09 11:42:58 -0500

    q

--- Java.js
+++ Java.js
@@ -1,4 +1,15 @@
 
-function saludar() {
-    console.log("¡Hola! Este es un saludo desde JavaScript.");
-}
+    const imagen = document.getElementById("imagen");
+    const imagenes =["imagenes/cesar.jpg","imagenes/PerroChido.png"];
+    let indice = 0;
+
+    imagen.addEventListener("click", () => {
+        imagen.classList.add("cambiando");
+        setTimeout(() => {
+            indice = (indice + 1) % imagenes.length
+            imagen.src = imagenes[indice];
+            setTimeout(() => {
+                imagen.classList.remove("cambiando");
+            }, 100);
+        }, 300);
+    });
